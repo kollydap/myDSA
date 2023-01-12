3e46a2e14b4fe69d55fb8f7e3cfb1e205e842608
kollydap <dapkolly@gmail.com>
2023-01-12 16:33:16 +0100

    graph bfs

--- Graph.py
+++ Graph.py
@@ -10,14 +10,29 @@ class Graph:
         '''
         self.gdict[vertex].append(edge)
 
-dt = {"a":["b","c"],
-    "b":["a","d","e"],
-    "c":["a","e"],
-    "d":["b","e","f"],
-    "e":["d","f","c"],
-    "f":["d","e"]
-}
-gp = Graph(dt)
-gp1 = Graph()
-gp1.addEdge("a","k")
-print(gp1.gdict)
+    #bREATH FIRST TRAVERSAL 
+    def bfs(self, vertex):
+        visited = [vertex]
+        queue = [vertex]
+        while queue:
+            deVertex = queue.pop(0)
+            print(deVertex)
+            for adjacentVertex in self.gdict[deVertex]:
+                if adjacentVertex not in visited:
+                    visited.append(adjacentVertex)
+                    queue.append(adjacentVertex)
+
+
+
+customDict = { "a" : ["b","c"],
+            "b" : ["a", "d", "e"],
+            "c" : ["a", "e"],
+            "d" : ["b", "e", "f"],
+            "e" : ["d", "f", "c"],
+            "f" : ["d", "e"]
+               }
+
+
+
+g = Graph(customDict)
+g.dfs("a")
